94de44068a16c4a70f06181e626af9fee67114dc
Richard Aydan Fiuza Ost <122907674+aydanfiuza@users.noreply.github.com>
2023-05-25 23:06:02 -0300

    finish e remove concluídos

--- js/index.js
+++ js/index.js
@@ -1,25 +1,52 @@
 $(document).ready(function() {
-    var todoForm = $('#todo-form')
-
-    todoForm.on("submit", function(e) {
+    $('#todo-form').on('submit', function(e) {
         e.preventDefault()
 
-        var todoValue = $('#todo-input').val()
-
-        if (todoValue == '') {
-           alert('Insira uma tarefa corretamente.')
+        if ($('#todo-input').val() == '') {
+            alert('Insira uma tarefa corretamente.')
         } else {
-            console.log(todoValue)
+            createTask()
+        }    
+
+        // funções
+
+        function createTask() {
+            var todoDiv = $('<div></div>')
+            todoDiv.addClass('todo')
+
+            var task = $('<h3></h3>').text($('#todo-input').val())
+            todoDiv.append(task)
+
+            var todoList = $('#todo-list')
+            todoList.append(todoDiv)
+
+            var finishButton = $('<button></button>')
+            finishButton.addClass('finish-todo')
+            finishButton.html('<i class="fa-solid fa-check"></i>')
+
+            var editButton = $('<button></button>')
+            editButton.addClass('edit-todo')
+            editButton.html('<i class="fa-solid fa-pen"></i>')
+
+            var removeButton = $('<button></button>')
+            removeButton.addClass('remove-todo')
+            removeButton.html('<i class="fa-solid fa-xmark"></i>')
+
+            todoDiv.append(finishButton)
+            todoDiv.append(editButton)
+            todoDiv.append(removeButton)
+
+        // eventos
+
+        finishButton.on('click', () => {
+            const targetEl = finishButton.closest("div")
+            targetEl.toggleClass('done')
+        })
+
+        removeButton.on("click", () => {
+            const parentEl = removeButton.closest("div")
+            parentEl.remove()
+        })
         }
-
-        var todoDiv = $('<div></div>')
-        todoDiv.addClass("todo")
-
-        var task = $('<h3></h3>').text(todoValue)
-        todoDiv.append(task)
-
-        var todoList = $('#todo-list')
-
-        todoList.append(todoDiv)
     })
 })
